dc9e379dfbf6b83daeb408255da3a1c9887438df
Sebastian Phillips <sebastian.phillips@wsu.edu>
2025-03-15 16:56:11 -0700

    Added Home UI, Background, Title, User Info

--- stuff/src/pages/home.js
+++ stuff/src/pages/home.js
@@ -2,6 +2,8 @@ import "../app/globals.css";
 import { useState, useEffect } from 'react';
 import { supabase } from '../utils/supabase';
 import { useRouter } from 'next/router';
+import Image from "next/image";
+
 
 export default function Home() {
   const [user, setUser] = useState(null);
@@ -36,14 +38,62 @@ export default function Home() {
   };
 
   return (
-    <div className="">
-      <h1 className="text-blue-700">Home</h1>
-      {user ? (
-        <p>Logged in as {user.email}</p>
-      ) : (
-        <p>Loading...</p>
-      )}
-      <button onClick={handleLogout}>Log out</button>
+    <div className="MainPage">
+    <Image
+      src ="/Images/background-home(2).jpg"
+      alt="Background"
+      layout="fill"
+      objectFit="cover"
+      quality={100}
+      className="z-[-1]"
+      />
+
+    
+    
+      <div className=" flex space-x-4">
+      
+      
+      {/* <h2 className="text-2xl ml-100">Subheading</h2>
+      <p className="text-gray-200-center text-center text-2xl mt-10 ml-5">This is a paragraph with gray text.</p>
+      <span className="text-red-500">This is a span element.</span> */}
+
+      </div>
+
+      <div className="Title and User Info flex justify-between w-full">
+
+
+
+        <div className="User Info absolute top-0 left-0 flex justify-start items-start w-1/4 p-4">
+          
+          <div className="User">
+
+            {user ? (
+            <h2 className="text-2xl"> Logged in as {user.email}</h2>
+             ) : (
+              <p>Loading...</p>
+              )}
+
+          </div>
+
+
+          <div className="Logout absolute top-0 left-120 flex justify-center items-center w-1/4 bg-gray-200">
+          
+            <button onClick={handleLogout} className="text-2xl text-center">Log out</button>
+
+          </div>
+
+
+        </div>
+
+        <div className="Title absolute top-0 left-1/2 transform -translate-x-1/2 flex justify-center items-center">
+
+          <h1 className="text-7xl font-bold text-center self-center" style={{ fontFamily: 'Prism'}}>STUFF</h1>
+
+        </div>
+
+      </div>
+
+      
     </div>
   );
 }
