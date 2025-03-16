19320ad9e8a5951341673d59754efcdf343fe2a5
yolkboat <146778551+yolkboat@users.noreply.github.com>
2025-03-15 14:28:09 -1000

    Update supabase.js

--- stuff/src/utils/supabase.js
+++ stuff/src/utils/supabase.js
@@ -1,10 +1,8 @@
 // utils/supabaseClient.js
 import { createClient } from '@supabase/supabase-js';
 
-// Initialize Supabase client with environment variables
-const supabase = createClient(
-  process.env.NEXT_PUBLIC_SUPABASE_URL,
-  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
-);
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
-export default supabase;
+// Create the Supabase client
+export const supabase = createClient("https://mickvlmfxtescdcsltme.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1pY2t2bG1meHRlc2NkY3NsdG1lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEwNjY4NzMsImV4cCI6MjA1NjY0Mjg3M30.PP0n8fM9cpQdXr1avFUM1FpMmoynLJ0KUyUv_cS7M-0");
