6c9eda9eb0bc0b2cec362f3c720d96c1d27ce233
Sebastian Phillips <sebastian.phillips@wsu.edu>
2025-03-21 14:23:46 -0700

    Fixed supabase ulr and key

--- stuff/src/utils/supabase.js
+++ stuff/src/utils/supabase.js
@@ -5,4 +5,4 @@ const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
 
 // Create the Supabase client
-export const supabase = createClient("https://mickvlmfxtescdcsltme.supabase.co", "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Im1pY2t2bG1meHRlc2NkY3NsdG1lIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDEwNjY4NzMsImV4cCI6MjA1NjY0Mjg3M30.PP0n8fM9cpQdXr1avFUM1FpMmoynLJ0KUyUv_cS7M-0");
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
