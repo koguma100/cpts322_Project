07f5e1ba408a2ae69ef5950cafd9c7f678e81845
Felix-Roman <166555273+Felix-Roman@users.noreply.github.com>
2025-04-04 13:05:11 -0700

    Update next.config.mjs

--- stuff/next.config.mjs
+++ stuff/next.config.mjs
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+/**const nextConfig = {};*/
+
+const nextConfig = {
+    images: {
+      domains: ['mickvlmfxtescdcsltme.supabase.co'], // Add your Supabase domain here
+    },
+  };
 
 export default nextConfig;
+
+
